235bd7d1f663ba569daf1b739e0ff6b597f2c83c
clinton <clinton.begin@gmail.com>
2017-01-30 23:45:11 -0700

    added tar.gz packaging

--- commands/pkg.go
+++ commands/pkg.go
@@ -9,6 +9,8 @@ import (
 	"strings"
 	"io"
 	"fmt"
+	"archive/tar"
+	"compress/gzip"
 )
 
 var PackageCommand = cli.Command{
@@ -29,13 +31,28 @@ func pkg(c *cli.Context) error {
 		nameParts := strings.Split(project.Name, "/")
 		shortName := nameParts[len(nameParts) - 1:][0]
 		targetFile := fmt.Sprintf("%s-%s-%s.%s", shortName, project.Version, artifact.Classifier, artifact.Archive)
+		source := project.TargetPath(artifact.Classifier)
+		dest := project.TargetPath(targetFile)
+		temp := fmt.Sprintf("%s.tmp", dest)
+		fmt.Println(artifact.Archive)
 		switch artifact.Archive {
 		case "zip":
-			return zipDir(project.TargetPath(artifact.Classifier), project.TargetPath(targetFile))
+			err := zipDir(source, dest)
+			if err != nil {
+				return err
+			}
 		case "tar.gz":
-
+			err := tarDir(source, dest)
+			if err != nil {
+				return err
+			}
+			err = gzipFile(dest, temp)
+			if err != nil {
+				return err
+			}
+			os.Remove(dest)
+			os.Rename(temp, dest)
 		}
-
 	}
 	return nil
 }
@@ -92,3 +109,69 @@ func zipDir(source, target string) error {
 
 	return err
 }
+
+func tarDir(source, target string) error {
+	tarfile, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer tarfile.Close()
+
+	tarball := tar.NewWriter(tarfile)
+	defer tarball.Close()
+
+	return filepath.Walk(source,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if path != source {
+
+				header, err := tar.FileInfoHeader(info, info.Name())
+				if err != nil {
+					return err
+				}
+
+				header.Name = strings.TrimPrefix(strings.TrimPrefix(path, source), "/")
+
+				if err := tarball.WriteHeader(header); err != nil {
+					return err
+				}
+
+				if info.IsDir() {
+					return nil
+				}
+
+				file, err := os.Open(path)
+				if err != nil {
+					return err
+				}
+				defer file.Close()
+				_, err = io.Copy(tarball, file)
+				return err
+			}
+			return err
+		})
+}
+
+func gzipFile(source, target string) error {
+	reader, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+
+	writer, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer writer.Close()
+
+	archiver := gzip.NewWriter(writer)
+	//archiver.Name = filename
+	defer archiver.Close()
+
+	_, err = io.Copy(archiver, reader)
+	return err
+}
+
