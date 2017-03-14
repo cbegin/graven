a5790da954054e76acaa03faaa5ad9f5450ffe68
clinton <clinton.begin@gmail.com>
2017-03-13 22:56:07 -0600

    Added archivefilename assertion

--- domain/govendor_test.go
+++ domain/govendor_test.go
@@ -11,4 +11,5 @@ func TestLoadVendorFile(t *testing.T) {
 	g, err := LoadGovendorFile(p)
 	assert.NoError(t, err)
 	assert.Equal(t, 4, len(g.Packages))
+	assert.Equal(t, "github-com-fatih-color-9131ab34cf20d2f6d83fdc67168a5430d1c7dc23.zip", g.Packages[0].ArchiveFileName())
 }
