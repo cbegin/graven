7982fac00a253514801043fa7e4127047d7da34a
clinton <clinton.begin@gmail.com>
2017-04-05 23:37:37 -0600

    intermediate commit while working on git validation

--- commands/deploy.go
+++ commands/deploy.go
@@ -206,7 +206,7 @@ func verifyRepoState(project *domain.Project) error {
 
 	// Check if changes exist on server
 	if err := verifyGitState(func(stdout, stderr string) error {
-		fmt.Println(len(strings.Split(strings.TrimSpace(stderr), "\r\n")))
+		fmt.Println(len(strings.Split(strings.TrimSpace(stderr), "\n")))
 		return nil
 	}, project, "fetch", "--dry-run", remoteName, branchName); err != nil {
 		return err
