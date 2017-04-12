c73cfbaa4e3ba664702f65fcda07437c79699156
clinton <clinton.begin@gmail.com>
2017-04-12 09:05:29 -0600

    stopped test from blowing away config

--- config/config_test.go
+++ config/config_test.go
@@ -7,9 +7,11 @@ import (
 
 func TestShouldWriteAndReadProperty(t *testing.T) {
 	c := NewConfig()
+	c.configFileName = ".graven_test.yml"
 	c.Set("foo", "bar", "baz")
 	c.Write()
 	c2 := NewConfig()
+	c2.configFileName = ".graven_test.yml"
 	c2.Read()
 	assert.Equal(t, "baz", c2.Get("foo", "bar"))
 }
