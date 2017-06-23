0ea5dffb7b75d7af7bff0678934f7e09835879dc
clinton <clinton.begin@gmail.com>
2017-06-23 00:15:36 -0600

    added dep vendor tool

---
+++ vendortool/dep_vendortool.go
@@ -0,0 +1,56 @@
+package vendortool
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"path/filepath"
+
+	"github.com/cbegin/graven/domain"
+	"github.com/pelletier/go-toml"
+)
+
+type DepPackage struct {
+	Name     string `toml:"name,omitempty"`
+	Revision string `toml:"revision,omitempty"`
+}
+
+type DepVendorTool struct {
+	Imports []DepPackage `toml:"projects,omitempty"`
+}
+
+func (g *DepPackage) ArchiveFileName() string {
+	chars := []string{"/", "\\", "."}
+	name := g.Name
+	for _, c := range chars {
+		name = strings.Replace(name, c, "-", -1)
+	}
+
+	return fmt.Sprintf("%v-%v.zip", name, g.Revision)
+}
+
+func (g *DepPackage) PackagePath() string {
+	osIndependentPath := filepath.Join(strings.FieldsFunc(g.Name, func(r rune) bool { return r == '\\' || r == '/' })...)
+	return osIndependentPath
+}
+
+func (g *DepVendorTool) LoadFile(project *domain.Project) error {
+	vendorFilePath := project.ProjectPath("Gopkg.lock")
+	vendorFile, err := os.Open(vendorFilePath)
+	vendorFileBytes, err := ioutil.ReadAll(vendorFile)
+	err = toml.Unmarshal(vendorFileBytes, g)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (g *DepVendorTool) Dependencies() []PackageDepencency {
+	deps := make([]PackageDepencency, len(g.Imports))
+	for i, dx := range g.Imports {
+		d := dx
+		deps[i] = &d
+	}
+	return deps
+}
---
+++ vendortool/dep_vendortool_test.go
@@ -0,0 +1,18 @@
+package vendortool
+
+import (
+	"testing"
+
+	"github.com/cbegin/graven/domain"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadDepFile(t *testing.T) {
+	vendorTool := &DepVendorTool{}
+	p, err := domain.LoadProject("../hello/project.yaml")
+	assert.NoError(t, err)
+	err = vendorTool.LoadFile(p)
+	assert.NoError(t, err)
+	assert.Equal(t, 7, len(vendorTool.Dependencies()))
+	assert.Equal(t, "github-com-davecgh-go-spew-6d212800a42e8ab5c146b8ace3490ee17e5225f9.zip", vendorTool.Dependencies()[0].ArchiveFileName())
+}
